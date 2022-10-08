6cb574f14af4a3c2b352d2a303f1fe85e7547cbe
praveen <anabathula.praveen103@gmail.com>
2022-10-09 05:13:43 +0530

    Update home page content

--- src/homecontent.js
+++ src/homecontent.js
@@ -1,14 +1,44 @@
 export default function generatehomecontent() {
+  const homecontent = document.createElement('div');
+  homecontent.id = "home"
+
   const paragraphtitle = document.createElement('div');
-  paragraphtitle.id = "home"
-  paragraphtitle.textContent = "Home"
+  paragraphtitle.id = "paragraphtitle"
+  paragraphtitle.textContent = "Irasshai!!"
 
   const paragraph = document.createElement('div');
+  paragraph.id = "paragraph"
   paragraph.textContent = 
-  `If you are a weeb worth your salt, you must know what NGNL is about. The idea of this
-  gambling inn is simple, if you beat our cute employees at a game, you get your food for free, plus
-  a picture with the employee you beat.`
+  `At this 『 NGNL 』themed gaming inn, you get the chance to win a free meal if you beat our employee gamer at game
+  associated with the menu. You'll have to follow the Ten Covenants when playing the games. Asshente!!!`
+  const rules = document.createElement('div');
+  rules.classList.add("rules")
+  generateRuleDiv(rules_content, rules)
 
-  document.querySelector(".content").appendChild(paragraphtitle);
-  document.querySelector(".content").appendChild(paragraph);
+  homecontent.appendChild(paragraphtitle)
+  homecontent.appendChild(paragraph)
+  homecontent.appendChild(rules)
+
+
+  document.querySelector(".content").appendChild(homecontent);
 }
+
+function generateRuleDiv(array, parentDiv) {
+  array.forEach((rule) => {
+    const rule_div =  document.createElement('div');
+    rule_div.textContent = rule
+    parentDiv.appendChild(rule_div)
+  })
+}
+
+const rules_content = [`十の盟約 `, 
+`【一つ】この世界におけるあらゆる殺傷、戦争、略奪を禁ずる`,
+`【二つ】争いは全てゲームによる勝敗で解決するものとする`,
+`【三つ】ゲームには、相互が対等と判断したものを賭けて行われる`,
+`【四つ】「三」に反しない限り、ゲーム内容、賭けるものは一切を問わない`,
+`【五つ】ゲーム内容は、挑まれたほうが決定権を有する`,
+`【六つ】"盟約に誓って"行われた賭けは、絶対遵守される`,
+`【七つ】集団における争いは、全権代理者をたてるものとする`,
+`【八つ】ゲームの中の不正発覚は、敗北と見なす`,
+`【九つ】以上をもって神の名のもと絶対不変のルールとする`,
+`【十】みんななかよくプレイしましょう`]
